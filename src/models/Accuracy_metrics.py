e80457a52271007a067ffb6f0fa888cd5549675c
WKuppers <90250550+WKuppers@users.noreply.github.com>
2022-03-18 13:30:10 +0100

    Update Accuracy_metrics.py

--- src/models/Accuracy_metrics.py
+++ src/models/Accuracy_metrics.py
@@ -1,2 +1,19 @@
-def confusion matrix():
-  
+import numpy as np
+
+import sklearn.metrics as metrics
+
+def Confusion_Matrix(predictions, targets):
+
+    tpr = sum()
+
+    confusion_matrix = [tpr, fnr, fpr, tnr]
+
+return confusion_matrix
+
+def ROC_Plot():
+
+return roc
+
+def Coverage_Matrix ():
+
+return coverage_matrix
